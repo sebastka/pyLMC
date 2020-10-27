3603269e106aae14f1016fb07ba97d2bb68b7694
Sebastian Karlsen <sebastian@karlsen.fr>
2020-10-27 17:55:41 +0100

    Add comments

--- LMC.py
+++ LMC.py
@@ -1,5 +1,22 @@
+""" Finds all the neighbors of Cell (x, y).
+
+	Args:
+		x (int): Row.
+		y (int): Column.
+	
+	Returns:
+		list of Cells: A list containing all the neighbors Cells of Cell (x, y).
+"""
+
 class LMC:
 	def __init__(self, filename, save = False):
+		""" Initialize LMC machine
+
+			Args:
+				filename (str): file to parse
+				save (bool): to save machine code or not
+		"""
+
 		self._mailbox = [0 for x in range(100)]
 		self._program = []
 		self._labels = {}
@@ -8,8 +25,7 @@ class LMC:
 			'HLT': 000, 
 			'ADD': 100, 
 			'SUB': 200, 
-			'STA': 300, 
-			'STO': 300,
+			'STA': 300, 'STO': 300,
 			'LDA': 500, 
 			'BRA': 600, 
 			'BRZ': 700, 
@@ -24,6 +40,17 @@ class LMC:
 		self.toMailbox(save)
 
 	def openFile(self, filename, mode = 'r'):
+		""" Open and test if a file can be opened
+
+			Args:
+				filename (str): file to open
+				mode (str): mode to open with. Default i 'r'
+			
+			Returns:
+				file handle upon success
+				exits upon failure
+		"""
+
 		try:
 			return open(filename, mode)
 		except IOError:
@@ -31,21 +58,37 @@ class LMC:
 			exit(1)
 
 	def ParseFile(self, filename):
+		""" Parse opened file line by line and add each relevant line to _program
+
+			Args:
+				filename (str): file to parse
+		"""
+
 		f = self.openFile(filename)
 
 		i = 0
 		for line in f.readlines():
-			line = self.parseLine(self.trimLine(line))
+			line = self.parseLine(self.trimLine(line)) # Parse line
+
 			if line != None:
 				if line[0] != None:
-					self._labels.update({line[0]: i})
+					self._labels.update({line[0]: i}) # If line has a label, add to label list
 				
-				self._program.append(line)
+				self._program.append(line) # Save line to _program
 				i += 1
 		
 		f.close()
 	
 	def trimLine(self, line):
+		""" Remove comments, tabs and spaces and nl from line
+
+			Args:
+				line (str)
+			
+			Returns:
+				str: a stripped line or None if line does not contain code
+		"""
+
 		# Remove comments:
 		line = line.split('//')
 		line = line[0].split('#')
@@ -61,56 +104,76 @@ class LMC:
 			return None
 	
 	def parseLine(self, line):
+		""" Parse line
+
+			Args:
+				line: str
+			
+			Returns:
+				list: [label, instruction, address]
+		"""
+
 		if line == None:
 			return None
 		
 		cols = len(line)
-		label_from = None
+		label = None
 		instruction = None
-		label_to = None
+		address = None
 
-		if cols == 1:
+		if cols == 1: # If the line has one column, it's only an instruction
 			instruction = line[0].upper()
-		elif cols == 2:
+		elif cols == 2: # If two, it's either a label with instruction, or instruction with address
 			if line[0].upper() in self.instructionSet:
 				instruction = line[0].upper()
-				label_to = line[1].upper()
+				address = line[1].upper()
 			elif line[1].upper() in self.instructionSet:
-				label_from = line[0].upper()
+				label = line[0].upper()
 				instruction = line[1].upper()
-		elif cols == 3:
-			label_from = line[0].upper()
+		elif cols == 3: # If three, all of them
+			label = line[0].upper()
 			instruction = line[1].upper()
-			label_to = line[2].upper()
+			address = line[2].upper()
 
+		# If instruction is invalid, bail out
 		if instruction not in self.instructionSet:
 			print('Unknown instruction \'' + str(instruction) + '\'!')
 			exit(0)
 		
-		return [label_from, instruction, label_to]
+		return [label, instruction, address]
 
 	def toMailbox(self, save):
+		""" Save _program to mailbox with machine code
+
+			Args:
+				save (bool): save machine code in file or not
+		"""
+
 		i = 0
-		for line in self._program:
+		for line in self._program: # Loop through program (parsed file)
 			if line[1] == 'DAT':
-				if line[2]:
+				if line[2]: # DAT is not always initialized to 0
 					self._mailbox[i] = int(line[2])
 				else:
 					self._mailbox[i] = 0
 			else:
-				self._mailbox[i] = int(self.instructionSet[line[1]])
+				self._mailbox[i] = int(self.instructionSet[line[1]]) # mailbox = Instruction code
 
-				if line[2] in self._labels:
+				if line[2] in self._labels: # mailbox += address as label (if indicated)
 					self._mailbox[i] += int(self._labels[line[2]])
-				elif line[1] == 'LDA' and line[2].isnumeric():
+				elif line[1] == 'LDA' and line[2].isnumeric(): # Load an address explicitly
 					self._mailbox[i] += int(line[2])
 			
 			i += 1
 		
-		if save:
+		if save: # Save mailbox to code.txt
 			self.writeMailbox()
 
 	def writeMailbox(self):
+		""" 
+			Save mailbox to code.txt
+		"""
+
 		f = self.openFile('code.txt', 'w')
 
 		for i in self._mailbox:
@@ -119,6 +182,8 @@ class LMC:
 		f.close()
 
 	def run(self):
+		""" Run mailbox
+		"""
 		acumulator = 0
 		counter = 0
 
@@ -164,9 +229,6 @@ class LMC:
 				pass
 
 			counter += 1
-
-def printList(m_list):
-	i = 0
-	for el in m_list:
-		print('#' + str(i) + ':\t' + str(el))
-		i += 1
+		
+		print('Unexpected behaviour. Does your program halt?')
+		exit(1)
