ab538054b6443c3171305fe57b31af2f9bd00ae4
Sebastian Karlsen <sebastian@karlsen.fr>
2020-10-27 17:33:25 +0100

    Remove old code from main.py and add fib.lmc

--- main.py
+++ main.py
@@ -2,12 +2,9 @@
 from LMC import LMC
 
 def main():
-	lmc = LMC('lmc.txt', False) # True to save machinecode to code.txt
+	lmc = LMC('fib.lmc', False) # True to save machinecode to code.txt
 	
-	# Halt should exit the program
 	return lmc.run()
-
-###################### TEMP:
 
 ######################
 
