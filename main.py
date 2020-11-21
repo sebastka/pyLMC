a2cee69106f9d11542cd48aff5ff297628fe9acc
Sebastian Karlsen <sebastian@karlsen.fr>
2020-11-21 15:14:38 +0100

    Add argument parser

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
 #!/usr/bin/env python3
+import argparse
 from LMC import LMC
 
-def main():
-	#lmc = LMC('fib.lmc')
-	#lmc.saveMailbox('fib.mailbox')
+parser = argparse.ArgumentParser(
+	"pyLMC",
+	description='Little Man Computer interpretor in Python.'
+)
 
-	# Load mailbox
-	lmc = LMC('fib.mailbox', True)
+parser.add_argument(
+	'--verbose', '-v',
+	help = 'run verbosely',
+	dest = 'verbose',
+	action='store_true'
+)
 
-	# lmc.run(True) to run verbosely
-	return lmc.run()
+parser.add_argument(
+	'-r',
+	help = 'load and run lmc file',
+	dest = 'lmc_in',
+	action='store'
+)
+
+parser.add_argument(
+	'-s',
+	help = 'store mailbox to file',
+	dest = 'mb_out',
+	action='store'
+)
+
+parser.add_argument(
+	'-l',
+	help = 'load and run mailbox',
+	dest = 'mb_in',
+	action='store'
+)
+
+
+
+def main(args):
+	if args.lmc_in: # Loads asm file
+		lmc = LMC(args.lmc_in)
+		
+		if args.mb_out:
+			lmc.saveMailbox(args.mb_out)
+		
+		return lmc.run(args.verbose)
+	elif args.mb_in: # Load mailbox
+		lmc = LMC(args.mb_in, True)
+		return lmc.run(args.verbose)
+	else:
+		print('Usage: pyLMC -h')
+		return 1
 
 ######################
 
-exit(main())
+exit(main(parser.parse_args()))
